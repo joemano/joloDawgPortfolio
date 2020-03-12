634d2e2ec90436ccb31b15211975da3e6f16f584
Jobert Manosca <joberttm@hotmail.com>
2020-03-12 15:25:05 -0400

    api call works

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,22 @@
 const joloPortfolio = {};
 
-joloPortfolio.init = () => {
+const devToApiKey = 'Pdeaq3Sioao7t5CzjKfajjNT';
 
+joloPortfolio.getPosts = () => {
+  $.ajax({
+    url: 'https://dev.to/api/articles',
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      username: 'joemano'
+    }
+  }).then((response) => {
+    console.log(response);
+  });
+}
+
+joloPortfolio.init = function() {
+  this.getPosts();
 }
 
 $(() => {
